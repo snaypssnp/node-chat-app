7e83a6a3b4c74a6cc4238cf2b43c8f71c71017e4
Sergey Popov <snaypssnp@gmail.com>
2017-04-11 15:52:07 -0400

    Add newmessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,28 @@
+(function() {
+
+  const socket = io();
+
+  socket.on('connect', function () {
+    console.log('Connected to server');
+    
+
+    socket.emit('createMessage', {
+      from: 'Andrew',
+      text: 'Yup, that works for me.'
+    })
+  });
+
+  socket.on('disconnect', function () {
+    console.log('Disconnected from server');
+  });
+
+  socket.on('newEmail', function(email) {
+    console.log('New email' , email);
+  });
+
+  socket.on('newMessage', function(message) {
+    console.log('newMessage', message);
+  });
+
+}());
+
--- server/server.js
+++ server/server.js
@@ -12,6 +12,26 @@ const io = socketIO(server);
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'John',
+    text: 'See you then',
+    createAt: 123123
+  })
+
+  socket.emit('newEmail', {
+    from: 'mike@example.com',
+    text: 'Hey. What is going on',
+    createAt: 123
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+  });
+
+  socket.on('createEmail', (newEmail) => {
+    console.log('createEmail', newEmail);
+  });
+
   socket.on('disconnect', function() {
     console.log('User was disconnected');
   });
