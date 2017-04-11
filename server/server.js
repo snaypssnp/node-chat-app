c15e4a6f0621f8696be30bd729339117c2272397
Sergey Popov <snaypssnp@gmail.com>
2017-04-11 16:19:32 -0400

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -12,6 +12,18 @@ const io = socketIO(server);
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New User joined',
+    createAt: new Date().getTime()
+  })
+
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
     io.emit('newMessage', {
@@ -19,6 +31,11 @@ io.on('connection', (socket) => {
       text: message.text,
       createAt: new Date().getTime()
     });
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createAt: new Date().getTime()
+    // })
   });
 
   socket.on('createEmail', (newEmail) => {
