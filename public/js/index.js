21757c5209886b4ccafeecdf13bc31e783e49c66
Sergey Popov <snaypssnp@gmail.com>
2017-04-11 17:34:22 -0400

    add form for messages adn show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -1,21 +1,35 @@
 (function() {
+  'use strict';
+
+  const socket = io();
+
+  socket.on('connect', function () {
+    console.log('Connected to server');
+  });
+
+  socket.on('disconnect', function () {
+    console.log('Disconnected from server');
+  });
 
 
+  socket.on('newMessage', function(message) {
+    console.log('newMessage', message);
+    const li = $('<li></li>');
+    li.text(`${message.from} ${message.text}`);
 
+    $('#messages').append(li);
+  });
+  
+  $('#message-form').on('submit', function(e) {
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+      from: 'User',
+      text: $('[name=message]').val()
+    }, function() {
+
+    });
+  });
 }());
 
-const socket = io();
-
-socket.on('connect', function () {
-  console.log('Connected to server');
-});
-
-socket.on('disconnect', function () {
-  console.log('Disconnected from server');
-});
-
-
-socket.on('newMessage', function(message) {
-  console.log('newMessage', message);
-});
 
