9e2fd7595a1dec6163ff74254b15e8dc14964c11
Sergey Popov <snaypssnp@gmail.com>
2017-04-11 16:01:59 -0400

    Emit newMessage on createMesssage

--- public/js/index.js
+++ public/js/index.js
@@ -1,28 +1,21 @@
 (function() {
 
-  const socket = io();
 
-  socket.on('connect', function () {
-    console.log('Connected to server');
-    
-
-    socket.emit('createMessage', {
-      from: 'Andrew',
-      text: 'Yup, that works for me.'
-    })
-  });
-
-  socket.on('disconnect', function () {
-    console.log('Disconnected from server');
-  });
-
-  socket.on('newEmail', function(email) {
-    console.log('New email' , email);
-  });
-
-  socket.on('newMessage', function(message) {
-    console.log('newMessage', message);
-  });
 
 }());
+
+const socket = io();
+
+socket.on('connect', function () {
+  console.log('Connected to server');
+});
+
+socket.on('disconnect', function () {
+  console.log('Disconnected from server');
+});
+
+
+socket.on('newMessage', function(message) {
+  console.log('newMessage', message);
+});
 
--- server/server.js
+++ server/server.js
@@ -12,20 +12,13 @@ const io = socketIO(server);
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'John',
-    text: 'See you then',
-    createAt: 123123
-  })
-
-  socket.emit('newEmail', {
-    from: 'mike@example.com',
-    text: 'Hey. What is going on',
-    createAt: 123
-  });
-
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createAt: new Date().getTime()
+    });
   });
 
   socket.on('createEmail', (newEmail) => {
